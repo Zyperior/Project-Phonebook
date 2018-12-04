32f3c45649d4e56b680ca92854de1df165a25c9c
Öyvind Borgersen <oybo84@gmail.com>
2018-12-04 12:41:12 +0000

    FileHandler class added

---
+++ src/phonebook/storage/FileHandler.java
@@ -0,0 +1,19 @@
+package phonebook.storage;
+import phonebook.base.*;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileHandler {
+
+
+    // Reads input from file and returns an arraylist
+    public ArrayList<Contact> retrieveListFromFile(){
+        return null;
+    }
+
+    // Writes Arraylist to file
+    public void saveData(ArrayList<Contact> list){
+
+    }
+}
