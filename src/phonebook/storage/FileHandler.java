67214a485f50494faced1b037adc74ec38b23e0d
Öyvind Borgersen <oybo84@gmail.com>
2018-12-04 17:24:06 +0000

    Methods for writing and retrieving list from file added

--- src/phonebook/storage/FileHandler.java
+++ src/phonebook/storage/FileHandler.java
@@ -9,11 +9,27 @@ public class FileHandler {
 
     // Reads input from file and returns an arraylist
     public ArrayList<Contact> retrieveListFromFile(){
-        return null;
+        ArrayList<Contact> contacts = new ArrayList<>();
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("AdressBook.txt"))){
+            contacts = (ArrayList<Contact>) in.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return contacts;
     }
 
     // Writes Arraylist to file
     public void saveData(ArrayList<Contact> list){
-
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("AdressBook.txt"))){
+            out.writeObject(list);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
