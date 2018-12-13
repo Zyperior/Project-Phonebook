80c6ab82e1e97809bfa33c4b173be3a9c4ad1596
wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2018-12-13 20:27:17 +0000

    maven restructure

---
+++ src/test/java/phonebook/base/ContactTest.java
@@ -0,0 +1,15 @@
+package phonebook.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactTest {
+
+    @Test
+    public void testSomething(){
+        int a=0;
+        assertTrue(a==0);
+    }
+
+}
