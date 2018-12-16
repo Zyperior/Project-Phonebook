ee8ae60a547f8749ccfd9714885a2b376dd789a0
Gustav Ljungberg <g.ljungberg@live.se>
2018-12-16 01:29:02 +0100

    Added comments

--- src/main/java/phonebook/base/AdressBookFactory.java
+++ src/main/java/phonebook/base/AdressBookFactory.java
@@ -2,6 +2,8 @@ package phonebook.base;
 
 import java.util.ArrayList;
 
+//Factory for AdressBook objects 
+//Takes a string and returns a new AdressBook
 public class AdressBookFactory {
 
 	public static AdressBook createAdressBook(String category) {
