11a4cd05dc6ecdfb3295d7fa7ca9ef1390dad535
Öyvind Borgersen <oybo84@gmail.com>
2018-12-16 20:36:40 +0000

    Test modified

--- src/test/java/phonebook/storage/FileHandlerTest.java
+++ src/test/java/phonebook/storage/FileHandlerTest.java
@@ -11,13 +11,13 @@ public class FileHandlerTest {
     @Test
     void testFileHandler(){
         ArrayList<Contact> contacts = new ArrayList<>();
-        Contact one = new Contact("Hansen", "Hans", "Göteborg", "12345678");
+        Contact one = new Contact("Hansen", "Hans", "Oslo", "12345678");
         contacts.add(one);
         FileHandler.saveData(contacts, "testFileHandler");
         ArrayList<Contact> contacts2 = new ArrayList<>(FileHandler.retrieveListFromFile("testFileHandler"));
         assertEquals("Hansen",contacts2.get(0).getLastName());
         assertEquals("Hans",contacts2.get(0).getFirstName());
-        assertEquals("Göteborg",contacts2.get(0).getLocation());
+        assertEquals("Oslo",contacts2.get(0).getLocation());
         assertEquals("12345678",contacts2.get(0).getPhoneNumber());
     }
 }
